86bf97f5dfcc83083d2d7c4ae8d3976c2def4137
Giulia <108691332+devsgiulia@users.noreply.github.com>
2023-10-16 13:55:29 -0300

    Add files via upload
    sistema de bicicleta feito na aula de p2

---
+++ p2/p2/BicicletaDeMarcha.java
@@ -0,0 +1,39 @@
+    package p2;
+
+    public class BicicletaDeMarcha extends Bicicleta{
+        private int numMarchas;
+        private int marchaAtual;
+        
+        public BicicletaDeMarcha(double velocidadeMaxima, String modelo, Assento assento, int numMarchas){
+            super(velocidadeMaxima, modelo, assento);
+            this.numMarchas = numMarchas;
+            marchaAtual = 1;
+        }
+        
+        public int getNumMarchas(){
+            return numMarchas;
+        }
+        public int getMarchaAtual(){
+            return marchaAtual;
+        }
+        public void setMarcha(int nova){
+            marchaAtual = nova;//checar o maximo (numMarchas) e o minimo (0); 
+        }
+    
+        @Override
+        public void acelera() {
+            super.velocidadeAtual = (super.getVelocidadeAtual() + (marchaAtual/3.0) > super.getVelocidadeMaxima()) ? super.getVelocidadeMaxima() : super.getVelocidadeAtual() + (marchaAtual/3.0);
+        }
+        @Override
+        public void desacelera() {
+            super.velocidadeAtual = (super.getVelocidadeAtual() - (marchaAtual/3.0) < 0) ? 0 : super.getVelocidadeAtual() - (marchaAtual/3.0);
+        }
+        
+        public String toString() {
+            return super.toString()
+                    + "\nMarcha atual: " + this.marchaAtual
+                    + "\nNumero de marchas: " + this.numMarchas;
+            
+        }
+    }
+
